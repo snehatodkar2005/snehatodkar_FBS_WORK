bf1eef153971aa9f4c7a294e9292fd7b915ec3ad
snehatodkar2005 <snehatodkar2005@gmail.com>
2025-06-16 22:40:52 +0530

    Add files via upload

---
+++ C Programming/Assignment/A3/sum of range.c
@@ -0,0 +1,15 @@
+void main()
+{
+	int start,end,rem;
+	int sum=0;
+	printf("enter the start and end value for sum");
+	scanf("%d%d",&start,&end);
+	int i=start;
+	while(i<=end)
+	{
+		sum=sum+i;
+		i++;
+	}
+	printf("sum of given range is %d",sum);
+	
+}
