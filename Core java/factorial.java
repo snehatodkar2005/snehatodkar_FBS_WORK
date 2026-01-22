9dd9b6c1777a47b4c6625d96e04d369ca158184a
snehatodkar2005 <snehatodkar2005@gmail.com>
2026-01-22 13:54:24 +0530

    Add files via upload

---
+++ Core java/factorial.java
@@ -0,0 +1,15 @@
+class test
+{
+public static void main(String[ ] args)
+{
+int num=5;
+int fact=1;
+int i=1;
+while(i<=num)
+{
+fact=fact*i;
+i++;
+}
+System.out.printf("factorial od given number is %d",fact);
+}
+}
---
+++ Core java/source.java
@@ -0,0 +1,16 @@
+class A
+{
+public static void main(String[ ] a)
+{
+System.out.println("hello");
+}
+
+}
+class B
+{
+public static void main(String[ ] a)
+{
+System.out.println("Bye");
+}
+
+}
